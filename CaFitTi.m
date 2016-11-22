a62ce91b955c75fae9177cd7807f609dad7e6bf8
Marcel Etienne Kaszap <lemek22@gmail.com>
2016-11-22 08:50:53 -0500

    MAJ de desktop

--- CaFitTi.m
+++ CaFitTi.m
@@ -1,7 +1,8 @@
 %
-% Ici on check si lapos peut fitter dans le moniteur
-% en fonction d'un point de référence donné par le coin
-% bas-gauche de la fenref
+% Ici on check si la position 'lapos' peut fitter dans le moniteur
+% en fonction d'un point de référence 'fenref' donné par le coin
+% bas-gauche
+% On retourne une position corrigé si nécesaire.
 %
 function V =CaFitTi(fenref, lapos)
   V =lapos;
@@ -9,8 +10,11 @@ function V =CaFitTi(fenref, lapos)
   monpos =get(0,'MonitorPositions');
   % on recherche dans quel moniteur on affiche
   for U =1:size(monpos, 1)
-    if (Laref >= monpos(U, 1)) & (Laref <= monpos(U, 3))
+    if (Laref >= monpos(U, 1)) && (Laref <= monpos(U, 3))
+    	% plus grand que la position gauche d'un moniteur et
+    	% plus petit que la position à droite de ce moniteur
       V(1) =min(max(monpos(U, 1), V(1)), monpos(U, 3)-V(3));
+      % on tient aussi compte de la bordure du haut de la fenêtre (25)
       V(2) =min(max(1, V(2)), monpos(U, 4)-monpos(U, 2)+1-V(4)-25);
     end
   end
