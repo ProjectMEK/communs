0fcb4de24b81e16a5b2c9d18f43d25cc9ecff615
Marcel Etienne Kaszap <lemek22@gmail.com>
2017-11-15 20:43:24 -0500

    MAJ 2017-11-15
    "Ré-adaptation Matlab-Octave" un pas vers l'affichage correct dans les
    deux environnements.

--- GUIJournal.m
+++ GUIJournal.m
@@ -10,16 +10,16 @@ function fig = GUIJournal(Ppa)
                  'CloseRequestFcn',@terminus,'MenuBar','none',...
                  'defaultuicontrolunits','Normalized',...
                  'DefaultUIControlBackgroundColor',[0.8 0.8 0.8],...
-                 'defaultUIControlFontSize',10);
+                 'defaultUIControlFontSize',10, 'userdata',{Ppa});
     posx=0.025; posy=0.2; large=1-2*posx; haut=0.95-posy;
     uicontrol('Parent',fig, 'tag','Editorial', 'BackgroundColor',[1 1 1], ...
               'Style','listbox', 'Position',[posx posy large haut], ...
               'String',Ppa.lesmots, 'Value',1);
     haut=0.075; posy=posy-2*haut; large=0.15; dx=0.1; posx=(1-dx-2*large)/2;
-    uicontrol('Parent',fig, 'Callback',@Ppa.reset, ...
+    uicontrol('Parent',fig, 'Callback',@onReset, ...
               'Position',[posx posy large haut], 'String','Mise à zéro');
     posx=posx+large+dx;
-    uicontrol('Parent',fig, 'Callback',@Ppa.sauve, 'Position',[posx posy large haut],...
+    uicontrol('Parent',fig, 'Callback',@onSauve, 'Position',[posx posy large haut],...
               'String','Sauvegarde', 'tooltipstring','Sauvegarder la journalisation');
 end
 
@@ -29,4 +29,21 @@ end
 %
 function terminus(varargin)
   set(gcf, 'visible','off');
+end
+
+%
+% on call la method "Ppa.reset"
+%
+function onReset(varargin)
+  H =get(gcf,'userdata');
+  class(H)
+  H.reset();
+end
+
+%
+% on call la method "Ppa.sauve"
+%
+function onSauve(varargin)
+  Ppa =get(gcf,'userdata');
+  Ppa.sauve();
 end
