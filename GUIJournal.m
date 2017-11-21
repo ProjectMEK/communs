23b058295ab4aa63ff80275a88099810d96471aa
Marcel Etienne Kaszap <lemek22@gmail.com>
2017-11-21 15:14:45 -0500

    MAJ 2017-11-21
    Plusieurs modif des GUI pour la compatibilité Matlab/Octave

--- GUIJournal.m
+++ GUIJournal.m
@@ -2,48 +2,44 @@
 % ***GUI POUR L'AFFICHAGE DU JOURNAL***
 %    MEK: août 2017
 %
+% Adapté pour Octave/Matlab
+%
 function fig = GUIJournal(Ppa)
     flarge =800; fhaut =300;
     lapos =[10 25 flarge fhaut];
     fig  =figure('Name','Journal de bord','tag', 'LeCrieur',...
                  'Resize','on', 'Position', lapos, ...
-                 'CloseRequestFcn',@terminus,'MenuBar','none',...
+                 'CloseRequestFcn',{@quienllama,'terminus'},'MenuBar','none',...
                  'defaultuicontrolunits','Normalized',...
                  'DefaultUIControlBackgroundColor',[0.8 0.8 0.8],...
-                 'defaultUIControlFontSize',10, 'userdata',{Ppa});
+                 'defaultUIControlFontSize',10);
+    setappdata(fig,'Ppa',Ppa);
     posx=0.025; posy=0.2; large=1-2*posx; haut=0.95-posy;
     uicontrol('Parent',fig, 'tag','Editorial', 'BackgroundColor',[1 1 1], ...
               'Style','listbox', 'Position',[posx posy large haut], ...
               'String',Ppa.lesmots, 'Value',1);
     haut=0.075; posy=posy-2*haut; large=0.15; dx=0.1; posx=(1-dx-2*large)/2;
-    uicontrol('Parent',fig, 'Callback',@onReset, ...
+    uicontrol('Parent',fig, 'callback',{@quienllama,'reset'}, ...
               'Position',[posx posy large haut], 'String','Mise à zéro');
     posx=posx+large+dx;
-    uicontrol('Parent',fig, 'Callback',@onSauve, 'Position',[posx posy large haut],...
+    uicontrol('Parent',fig, 'callback',{@quienllama,'sauve'}, 'Position',[posx posy large haut],...
               'String','Sauvegarde', 'tooltipstring','Sauvegarder la journalisation');
 end
 
-%
-% On efface pas le GUI, on le met invisible.
-% l'Objet qui le gère va s'occuper de l'effacer en temps et lieu.
-%
-function terminus(varargin)
-  set(gcf, 'visible','off');
-end
 
 %
-% on call la method "Ppa.reset"
+% on call la method "Ppa.(autre)"
 %
-function onReset(varargin)
-  H =get(gcf,'userdata');
-  class(H)
-  H.reset();
-end
-
-%
-% on call la method "Ppa.sauve"
-%
-function onSauve(varargin)
-  Ppa =get(gcf,'userdata');
-  Ppa.sauve();
+function quienllama(src,evt, autre)
+  Ppa =getappdata(gcf,'Ppa');
+  switch autre
+  case 'terminus'
+    % On efface pas le GUI, on le met invisible.
+    % l'Objet qui le gère va s'occuper de l'effacer en temps et lieu.
+    set(gcf, 'visible','off');
+  case 'reset'
+    Ppa.reset(src);
+  case 'sauve'
+    Ppa.sauve();
+  end
 end
