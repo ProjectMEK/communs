231faf79630ae2e4b9d65573b03e617080bc2914
Marcel Etienne Kaszap <lemek22@gmail.com>
2017-12-07 14:46:54 -0500

    MAJ 2017-12-07
    implémentation d'une solution temporaire pour uitable dans Octave

--- uitable_OCT.m
+++ uitable_OCT.m
@@ -17,9 +17,9 @@ function varargout = uitable_OCT(varargin)
   S.fontname ='FixedWidth';
   S.userdata =0;
   S.columneditable =true;
+  S.rowname ={'1'};
   S.columnname ={'1'};
   S.columnwidth =0;
-  S.rowname ={'1'};
   S.backgroundcolor =[1 1 1];
   S.data =0;
   % maintenant on va peupler cette structure avec le contenu de varargin
@@ -33,7 +33,31 @@ function varargout = uitable_OCT(varargin)
   % calcul des dimensions de la uitable en fonction du 'data'
   S.nrow =size(S.data,1);
   S.ncol =size(S.data,2);
+  % Nom des colonnes
+  if length(S.columnname) ~= S.ncol
+    S.columnname ={};
+    for U =1:S.ncol
+      S.columnname{end+1} =num2str(U);
+    end
+  end
+  % Nom des rangées
+  if length(S.rownname) ~= S.nrow
+    S.rownname ={};
+    for U =1:S.nrow
+      S.rownname{end+1} =num2str(U);
+    end
+  end
   % fabrication du cadre
   ppa =uipanel('parent',S.parent,'position',S.position,'tag',S.tag,'title','','BorderType','beveledin');
-
+  % calcul des dimensions des cellules
+  if isa(S.columnwidth,'cell')
+    S.columnwidth =S.columnwidth{1};
+  end
+  % Si l'usager a donné une valeur, on la conserve. reste à calculer la 1ère colonne
+  lpan =S.position(3);
+  larg1 =lpan-S.columnwidth*S.ncol;
+  if larg1 < 1
+    % l'usager n'a rien donné. On va calculer la largeur des cell et la 1ère colonne
+    
+  end
 end
